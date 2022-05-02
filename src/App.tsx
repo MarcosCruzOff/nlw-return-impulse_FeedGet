7fb3cc37d020087fc6bfe9d621f525a4b194b77b
MarcosCruzOff <marcoscruz_off@outlook.com>
2022-05-02 19:44:31 -0300

    started impulse

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Widget } from './components/widget'
+
+export function App() {
+    return <Widget />
+}
+
+// import { useState } from 'react'
+
+// function App() {
+//   return <h1>Hello world</h1>
+// }
+
+// export default App
